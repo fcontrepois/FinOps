99a9330fd1ad7e634fd29fd53941fd0ae1fc000f
Frank Contrepois <frank@coblan.it>
2023-08-30 15:46:50 +0200

    Copying the kitchen sink env

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,12 +1,1 @@
 $.index.open();
-
-var dialog = require('ti.webdialog');
-
-function followLink(event){
-    var selectUrl = event.source.link;
-    if (dialog.isSupported()) {
-        dialog.open({
-            url: selectUrl
-        });
-    }
-}
